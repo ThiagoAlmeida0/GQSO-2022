eddadea150a8d3728187d2b35de75113be6306b9
Thiago Almeida <thiago11contato@gmail.com>
2022-06-21 15:27:37 -0300

    função multiplicação

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ console.log('rodando');
 
 app.get("/sub/:x/:y", (req, res) => {
     var sub = parseInt (req.params.x)-parseInt(req.params.y)
-    res.send("O valor da subtração é:"+sub)
+    res.send("O valor da subtração é: "+sub)
 })
 
 
 app.get("/div/:x/:y", (req, res) => { 
     var div = parseInt(req.params.x)/parseInt(req.params.y)
-    res.send("O valor da divisão é:"+div)
+    res.send("O valor da divisão é: "+div)
+})
+
+app.get("/multiplicação/:x/:y", (req, res) => { 
+    var multiplicação = parseInt(req.params.x)*parseInt(req.params.y)
+    res.send("O valor da mutiplicação é: "+multiplicação)
 })
 
 app.listen(port, ()=>{

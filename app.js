c5d3f450b5403c5c19c9b9ac7a2445ae4ba10525
juliarodrigues10 <jvrm1@aluno.ifal.edu.br>
2022-06-28 14:01:50 +0000

    Alterações realizadas.

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.get("/div/:x/:y", (req, res) => {
     var div = parseInt(req.params.x)/parseInt(req.params.y)
     res.send("O valor da divisão é:"+div)
 })
-app.get ("/soma/:x/:y", (req,res) => {
-    var soma = parseInt(req.params.x)+parseInt(req.params.y)
-    res.send ("O valor da soma é:" +soma)
+app.get ("/som/:x/:y", (req,res) => {
+    var som = parseInt(req.params.x)+parseInt(req.params.y)
+    res.send ("O valor da soma é:" +som)
 })
 
 app.listen(port, ()=>{

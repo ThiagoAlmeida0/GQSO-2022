fa841676f0462cece52550b3b35b5a8f3ec4e75d
Thiago Almeida <thiago11contato@gmail.com>
2022-07-01 23:29:03 -0300

    tratamento de error 400

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
 const express = require("express");
-const app = express()
-const port = 7070
+const app = express();
+const port = 7070;
 
+console.log("rodando");
 
-console.log('rodando');
-
-
-app.get('/', (req, res) => {
-    res.render("index")
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 function converterUrlParaNumero(urlString, numero) {
-    for (let i = 0; i < urlString.length; i++) {
-        if (parseInt(urlString.charAt(i)) != undefined) {
-            if(urlString.charAt(i) == "/"){
-                break;
-            }
-            numero = numero + urlString.charAt(i)
-        }
-        console.log("primeiro numero: "+numero);
-        return parseInt(numero);
-    }
-    return 0;
-}
-
-app.use(function (req, res, next) {
-    let teste = req.url.slice(5);
-    console.log(teste);
-    let primeiroNumero = teste
-   // let segundoNumero = ""
-    primeiroNumero = converterUrlParaNumero(primeiroNumero);
-    //segundoNumero = converterUrlParaNumero(segundoNumero)
-    if (req.url != "/sub/:x/:y") {
-        console.log("Errouuuuu")
-        next()
+  let num = "";
+  if (!urlString) {
+    return undefined;
+  }
+  for (let i = 0; i < urlString.length; i++) {
+    if (parseInt(urlString.charAt(i)) != undefined) {
+      if (urlString.charAt(i) == "/") {
+        break;
+      } else {
+        num = num + urlString.charAt(i);
+      }
     } else {
-
-        console.log("nao errou");
-        next()
+      console.log("argumento não é inteiro. indefinido.");
     }
-
+  }
+  return num;
 }
-)
 
+app.use((req, res, next) => {
+  let teste = req.url.slice(5);
+  let primeiroNumero = teste;
+  primeiroNumero = converterUrlParaNumero(primeiroNumero);
+  aux = req.url.slice(5).split("/");
 
+  let segundoNumero = "";
+  segundoNumero = converterUrlParaNumero(aux[1]);
+  if (!primeiroNumero || !segundoNumero) {
+    return res.status(400).send("ERRO 400 BAD REQUEST" );
+  }
+
+ // if (primeiroNumero && segundoNumero) {
+ //   console.log("primeiro e segundo numero existem");
+ // } else {
+ //   console.log("primeiro ou segundo nao existem");
+ //   console.log("pri " + primeiroNumero + "\nseg " + segundoNumero);
+ // }
+
+  next();
+});
 
 app.get("/sub/:x/:y", (req, res) => {
-    var sub = parseInt(req.params.x) - parseInt(req.params.y)
-    res.send("O valor da subtração é:" + sub)
-})
-
+  var sub = parseInt(req.params.x) - parseInt(req.params.y);
+  res.send("O valor da subtração é:" + sub);
+});
 
 app.get("/div/:x/:y", (req, res) => {
 
@@ -58,14 +61,14 @@ app.get("/div/:x/:y", (req, res) => {
         res.send("O valor da divisão é:" + div)
     }
     else {
-        res.status(400).end()
+        res.status(400).send("ERROR 400 BAD REQUEST DIVISAO POR ZERO ")
     }
 })
 app.get("/som/:x/:y", (req, res) => {
-    var som = parseInt(req.params.x) + parseInt(req.params.y)
-    res.send("O valor da soma é: " + som)
-})
+  var som = parseInt(req.params.x) + parseInt(req.params.y);
+  res.send("O valor da soma é: " + som);
+});
 
 app.listen(port, () => {
-    console.log("rodou")
-}) 
+  console.log("rodou");
+});

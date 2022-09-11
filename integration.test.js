c7de651f6442e0de3741131fe5870a4d975f3f6b
juliarodrigues10 <jvrm1@aluno.ifal.edu.br>
2022-09-11 01:50:05 +0000

    adicionado teste de integração de subtração

--- integration.test.js
+++ integration.test.js
@@ -11,4 +11,11 @@ describe("Teste meu servidor", () => {
         expect(response.body).toEqual({ resultado: "O valor da soma é: 3" })
 
     })
+    it("deve retonar a subtracao corretamente ", async () => {
+
+        const response = await request(app)
+            .get("/sub/10/2")
+        expect(response.body).toEqual({ resultado: "O valor da subtração é:8" })
+
+    })
 })
